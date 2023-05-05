4e2ad65b73c1ecd0f6633b4401d5c80f15eecf28
Toy <h123@gmail.com>
2023-05-04 23:09:52 -0600

    beginning of heap

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -1,16 +1,49 @@
-#include "heap.h"
+#include "../heap/heap.h"
+
 
 Heap::Heap(int size) {
+    this->index = 0;
     this->size = size;
-    heap = new State[size];
+    heap = new *State[size];
 }
 
 Heap::~Heap() {
     delete[] heap;
 }
 
-Heap::insert(){
-    
+State *Heap::getMaxRoot() {
+    return heap[0];
 }
-    
+
+State *Heap::lChild(int index) {
+    return heap[2*index + 1];
+}
+
+State *Heap::rChild(int index) {
+    return heap[2*index + 2];
+}
+
+void Heap::maxHeapify(int i){
+
+    State *r = rChild(i);
+    State *l = lChild(i);
+
+    largest = i;
+
+    if(l < heap_size && this.heap[l] > this.heap[i]) {
+        largest = l;
+    }
+
+    if(r < heap_size && this.heap[r] > this.heap[largest]) {
+        largest = r;
+    }
+
+    if(largest != i) {
+        swap(this.heap[i], this.heap[largest]);
+        maxHeapify(largest);
+    }
+
+
+}
+
 
--- heap/heap.h
+++ heap/heap.h
@@ -3,15 +3,28 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+
+//todo: definir todas las operaciones por realizar en el heap
+// entender max heapify y build max heap
+
+
+
 class Heap {
     private:
-        State *heap;
-        int size;    
-    
+        State **heap;
+        int index;
+        int maxSize;    
+
     public:
+    
         Heap(int size);
         ~Heap();
-        void insert(int value);
+
+        State getMaxRoot();
+        State *lChild(int index);
+        State *rChild(int index);
+        
+        void maxHeapify();
         void remove();
         void print();
     
